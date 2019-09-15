1dc54d213ed793d6e790c8c6e09e564f005c6084
Bethel Eyo <betheleyo@gmail.com>
2019-09-15 23:59:48 +0100

    main commit

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body
+        ];
+    }
+
+    public function with($request){
+        // to add additional stuffs to the data
+        return [
+            'version' => '1.0.2',
+            'author_url' => 'http://codingisfun.com'
+        ];
+    }
+}
